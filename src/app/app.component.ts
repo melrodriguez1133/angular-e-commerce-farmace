ac36f0a0c66646adb8ccc96365edbe3945d87e5d
melrodriguez1133 <202000231@est.umss.edu>
2024-07-26 01:30:00 -0400

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from "./header/header.component";
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent,ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
